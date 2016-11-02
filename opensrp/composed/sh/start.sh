99ec419351188bb0d4dc7b80580130fb50fea738
Ephraim Muhia <emuhia@ona.io>
2016-11-02 15:11:23 +0300

    Revert "EM| removed sql files"
    This reverts commit 434b9dbb1710f03c855494ee59ec6e802b91594f.

--- opensrp/composed/sh/start.sh
+++ opensrp/composed/sh/start.sh
@@ -7,10 +7,10 @@ MYSQL_HOST=mysql
 /usr/local/bin/wait-for-it.sh ${MYSQL_HOST}:3306
 
 mysql -u root -p"$MYSQL_ROOT_PASSWORD" -h "$MYSQL_HOST" -e "CREATE DATABASE $MYSQL_MOTECH_DATABASE /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" -h "$MYSQL_HOST" "$MYSQL_MOTECH_DATABASE" < "/root/sql/tables_quartz_mysql.sql"
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" -h "$MYSQL_HOST" "$MYSQL_OPENMRS_DATABASE" < "/root/sql/openmrs.sql"
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" -h "$MYSQL_HOST" "$MYSQL_OPENMRS_DATABASE" < "/root/sql/locations.sql"
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" -h "$MYSQL_HOST" "$MYSQL_OPENMRS_DATABASE" < "/root/sql/person_attribute_type.sql"
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -h "$MYSQL_HOST" "$MYSQL_MOTECH_DATABASE" < "~/sql/tables_quartz_mysql.sql"
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -h "$MYSQL_HOST" "$MYSQL_OPENMRS_DATABASE" < "~/sql/openmrs.sql"
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -h "$MYSQL_HOST" "$MYSQL_OPENMRS_DATABASE" < "~/sql/locations.sql"
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -h "$MYSQL_HOST" "$MYSQL_OPENMRS_DATABASE" < "~/sql/person_attribute_type.sql"
 
 # create openmrs properties file
 touch /root/.OpenMRS/openmrs-runtime.properties
